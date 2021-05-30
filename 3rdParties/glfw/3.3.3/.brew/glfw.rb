d383f819007532043e358bfc0e9357ca0e938af2
Nikolay Fedotenko <kalkolay@protonmail.com>
2021-05-31 01:19:25 +0300

    Remove Homebrew in CI

---
+++ 3rdParties/glfw/3.3.3/.brew/glfw.rb
@@ -0,0 +1,40 @@
+class Glfw < Formula
+  desc "Multi-platform library for OpenGL applications"
+  homepage "https://www.glfw.org/"
+  url "https://github.com/glfw/glfw/archive/3.3.3.tar.gz"
+  sha256 "aa9922b55a464d5bab58fcbe5a619f517d54e3dc122361c116de573670006a7a"
+  license "Zlib"
+  head "https://github.com/glfw/glfw.git"
+
+  depends_on "cmake" => :build
+
+  def install
+    args = std_cmake_args + %w[
+      -DGLFW_USE_CHDIR=TRUE
+      -DGLFW_USE_MENUBAR=TRUE
+      -DBUILD_SHARED_LIBS=TRUE
+    ]
+
+    system "cmake", *args, "."
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #define GLFW_INCLUDE_GLU
+      #include <GLFW/glfw3.h>
+      #include <stdlib.h>
+      int main()
+      {
+        if (!glfwInit())
+          exit(EXIT_FAILURE);
+        glfwTerminate();
+        return 0;
+      }
+    EOS
+
+    system ENV.cc, "test.c", "-o", "test",
+                   "-I#{include}", "-L#{lib}", "-lglfw"
+    system "./test"
+  end
+end
